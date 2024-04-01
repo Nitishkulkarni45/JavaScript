3fb2b0cdb75c11f88835dc0c9494010d921aa942
Nitishkulkarni45 <nitishdkulkarnisonu13122001@gmail.com>
2024-04-01 20:55:21 +0530

    str concat and str length used for username generation

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,13 +1,16 @@
-console.log('im learning js');
-let fullName = prompt("enter your name");
-let namee = ' ';
-for(let val of fullName){
-    if(val !== 'i'){
-        namee = namee + val;
-    }
-    else{
-        namee = namee + 'i';
-        namee = namee + 'a';
-    }
-    console.log("name =",namee);
-}
+// console.log('im learning js');
+// let fullName = prompt("enter your name");
+// let namee = ' ';
+// for(let val of fullName){
+//     if(val !== 'i'){
+//         namee = namee + val;
+//     }
+//     else{
+//         namee = namee + 'i';
+//         namee = namee + 'a';
+//     }
+//     console.log("name =",namee);
+// }
+let fullName = prompt("Enter you name");
+let userName = "@"+fullName+fullName.length;
+console.log(userName);
